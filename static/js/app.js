ffb278ef4010926e84a612343a8cb6e40b41a142
ybacoder <ybacoder@gmail.com>
2020-03-06 22:19:15 -0600

    Allow filtering by date, city, state, country, and shape

--- static/js/app.js
+++ static/js/app.js
@@ -11,39 +11,29 @@ const handler = function () {
     let country = d3.select("#country").property("value")
     let shape = d3.select("#shape").property("value")
 
-    if (datetime) {
-        console.log(datetime)
-    }
-
-    if (city) {
-        console.log(city)
-    }
-
-    if (state) {
-        console.log(state)
-    }
-
-    if (country) {
-        console.log(country)
-    }
-
-    if (shape) {
-        console.log(shape)
-    }
-    
     let filterData = tableData
 
+    if (datetime) {console.log(datetime); filterData = filterData.filter(e => e.datetime == datetime)}
+
+    if (city) {console.log(city); filterData = filterData.filter(e => e.city == city)}
+
+    if (state) {console.log(state); filterData = filterData.filter(e => e.state == state)}
+
+    if (country) {console.log(country); filterData = filterData.filter(e => e.country == country)}
+
+    if (shape) {console.log(shape); filterData = filterData.filter(e => e.shape == shape)}
+    
     tbody.selectAll("*").remove()
 
     filterData.forEach(e => {
-    let row = tbody.append("tr")
-    row.append("td").text(e.datetime)
-    row.append("td").text(e.city.charAt(0).toUpperCase() + e.city.substring(1))
-    row.append("td").text(e.state.toUpperCase())
-    row.append("td").text(e.country.toUpperCase())
-    row.append("td").text(e.shape.charAt(0).toUpperCase() + e.shape.substring(1))
-    row.append("td").text(e.durationMinutes)
-    row.append("td").text(e.comments)
+        let row = tbody.append("tr")
+        row.append("td").text(e.datetime)
+        row.append("td").text(e.city.charAt(0).toUpperCase() + e.city.substring(1))
+        row.append("td").text(e.state.toUpperCase())
+        row.append("td").text(e.country.toUpperCase())
+        row.append("td").text(e.shape.charAt(0).toUpperCase() + e.shape.substring(1))
+        row.append("td").text(e.durationMinutes)
+        row.append("td").text(e.comments)
     })
 }
 
