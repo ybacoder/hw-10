982f3b99f3e27741865ddc8d427ef74e28a89f1b
ybacoder <ybacoder@gmail.com>
2020-03-06 21:29:44 -0600

    Add id for table body and edit style sheet to center data

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 const tableData = data;
 
-// YOUR CODE HERE!
+const tbody = d3.select("#ufo-table-tbody")
+
+filterData = tableData
+
+filterData.forEach(e => {
+    let row = tbody.append("tr")
+    row.append("td").text(e.datetime)
+    row.append("td").text(e.city.charAt(0).toUpperCase() + e.city.substring(1))
+    row.append("td").text(e.state.toUpperCase())
+    row.append("td").text(e.country.toUpperCase())
+    row.append("td").text(e.shape.charAt(0).toUpperCase() + e.shape.substring(1))
+    row.append("td").text(e.durationMinutes)
+    row.append("td").text(e.comments)
+})
+
+
